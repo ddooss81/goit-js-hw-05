4365f33d11a3f1ce151189f4957e2cc41116e79d
ddooss81 <144255208+ddooss81@users.noreply.github.com>
2024-01-10 14:23:44 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,357 +1,81 @@
-// 'use strict';
+`use strict`;
+users = ([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male"
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female"
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male"
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female"
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male"
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male"
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female"
+    }
+]);
 
-// function deliverPizza(pizzaName) {
-//   return `Delivering ${pizzaName} pizza.`;
-// }
-
-// function makePizza(pizzaName) {
-//   return `Pizza ${pizzaName} is being prepared, please wait...`;
-// }
-
-// function makeMessage(pizzaName, callBack) {
-  
-//     return callBack(pizzaName);
-// }
-
-// console.log(makeMessage("Royal Grand", makePizza));
-// console.log(makeMessage("Ultracheese", deliverPizza));
-
-// function makePizza(pizzaName, callback) {
-//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
-//   callback(pizzaName);
-// }
-
-// makePizza("Royal Grand", function deliverPizza(pizzaName) {
-//   console.log(`Delivering pizza ${pizzaName}`);
-// });
-
-// makePizza("Ultracheese", function deliverPizza(pizzaName) {
-//   console.log(`Delivering pizza ${pizzaName}`);
-// });
-
-
-// makePizza("Royal Grand");
-// makePizza("Ultracheese");
-
-// function calculateTotalPrice(orderedItems) {
-//   let totalPrice = 0;
-
-//   orderedItems.forEach(function(elem) {
-//     totalPrice += elem;
-//   });
-
-//   return totalPrice;
-// }
-// console.log(calculateTotalPrice([12, 85, 37, 4]));
-// console.log(calculateTotalPrice([164, 48, 291]));
-// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
-
-// function filterArray(numbers, value) {
-//   let result = [];
-
-//   numbers.forEach((number) => {
-//     if (number > value) {
-//       result.push(number);
-//     }
-//   });
-
-//   return result;
-// }
-
-// console.log(filterArray([1, 2, 3, 4, 5], 3));
-// console.log(filterArray([1, 2, 3, 4, 5], 4));
-// console.log(filterArray([1, 2, 3, 4, 5], 5));
-// console.log(filterArray([12, 24, 8, 41, 76], 38));
-// console.log(filterArray([12, 24, 8, 41, 76], 20));
-
-// const calculateTotalPrice = (quantity, pricePerItem) => {
-//   return quantity * pricePerItem;
-// };
-
-// console.log(calculateTotalPrice(5, 100));
-// console.log(calculateTotalPrice(8, 60));
-// console.log(calculateTotalPrice(3, 400));
-// let totalPrice = 0;
-// const calculateTotalPrice = (orderedItems) => {
-//   orderedItems.forEach((item) => {totalPrice += item;
-//   });
-//   return totalPrice;
-// };
-  
-// console.log(calculateTotalPrice([12, 85, 37, 4]));
-// console.log(calculateTotalPrice([164, 48, 291]));
-// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
-// function calculateTotalPrice(orderedItems) {
-//   let totalPrice = 0;
-
-// orderedItems.forEach((item) => {
-//   totalPrice += item;
-// });
-//   return totalPrice;
-// }
-
-    
-//     console.log(filterArray([1, 2, 3, 4, 5], 3));
-//     console.log(filterArray([1, 2, 3, 4, 5], 4));
-//     console.log(filterArray([12, 24, 8, 41, 76], 38));
-//     console.log(filterArray([12, 24, 8, 41, 76], 20));
-    
-// function changeEven(numbers, value) {
-//   let result = [];
-//   for (let i = 0; i < numbers.length; i += 1) {
-//     if (numbers[i] % 2 === 0) {
-//   result.push(numbers[i] + value);
-// } else {
-//   result.push(numbers[i]);
-// }
-//     }
-//   }
-
-// const books = [
-//   {
-//     title: "The Last Kingdom",
-//     author: "Bernard Cornwell",
-//     rating: 8.38,
-//   },
-//   {
-//     title: "Beside Still Waters",
-//     author: "Robert Sheckley",
-//     rating: 8.51,
-//   },
-//   {
-//     title: "The Dream of a Ridiculous Man",
-//     author: "Fyodor Dostoevsky",
-//     rating: 7.75,
-//   },
-//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-// ];
-
-// const titles = books.map(title => title.title);
-
-// console.log(titles);
-
-// const books = [
-//   {
-//     title: "The Last Kingdom",
-//     author: "Bernard Cornwell",
-//     genres: ["adventure", "history"],
-//   },
-//   {
-//     title: "Beside Still Waters",
-//     author: "Robert Sheckley",
-//     genres: ["fiction"],
-//   },
-//   {
-//     title: "Redder Than Blood",
-//     author: "Tanith Lee",
-//     genres: ["horror", "mysticism"],
-//   },
-// ];
-
-// const genres = books.flatMap(gen => gen.title);
-// console.log(genres);
-
-// const users = [
-//   {
-//     name: "Moore Hensley",
-//     email: "moorehensley@indexia.com",
-//     eyeColor: "blue",
-//     friends: ["Sharron Pace"],
-//     isActive: false,
-//     balance: 2811,
-//     skills: ["ipsum", "lorem"],
-//     gender: "male",
-//     age: 37,
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     email: "sharlenebush@tubesys.com",
-//     eyeColor: "blue",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     isActive: true,
-//     balance: 3821,
-//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-//     gender: "female",
-//     age: 34,
-//   },
-//   {
-//     name: "Ross Vazquez",
-//     email: "rossvazquez@xinware.com",
-//     eyeColor: "green",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     isActive: false,
-//     balance: 3793,
-//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-//     gender: "male",
-//     age: 24,
-//   },
-//   {
-//     name: "Elma Head",
-//     email: "elmahead@omatom.com",
-//     eyeColor: "green",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     isActive: true,
-//     balance: 2278,
-//     skills: ["adipisicing", "irure", "velit"],
-//     gender: "female",
-//     age: 21,
-//   },
-//   {
-//     name: "Carey Barr",
-//     email: "careybarr@nurali.com",
-//     eyeColor: "blue",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     isActive: true,
-//     balance: 3951,
-//     skills: ["ex", "culpa", "nostrud"],
-//     gender: "male",
-//     age: 27,
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     email: "blackburndotson@furnigeer.com",
-//     eyeColor: "brown",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     isActive: false,
-//     balance: 1498,
-//     skills: ["non", "amet", "ipsum"],
-//     gender: "male",
-//     age: 38,
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     email: "shereeanthony@kog.com",
-//     eyeColor: "brown",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     isActive: true,
-//     balance: 2764,
-//     skills: ["lorem", "veniam", "culpa"],
-//     gender: "female",
-//     age: 39,
-//   },
-// ];
-
-// console.log(users);
-
-// const getUserEmails = (users) => {
-//   return users.map((user) => user.email);
-// };
-// console.log(getUserEmails);
-
-// const emails = (users) => {
-//   return users.map((user) => user.email);
-// };
-
-// const listOfEmails = emails(users);
-
-// console.log(listOfEmails);
+const sortByDescendingFriendCount = users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 
-// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+console.log(sortByDescendingFriendCount);
 
-// const evenNumbers = numbers.filter((namberPlus) => {return namberPlus % 2 === 0});
-// const oddNumbers = numbers.filter((namberPlus) => {return namberPlus % 2 !== 0});
-
-// console.log(evenNumbers);
-// console.log(oddNumbers);
-
-// const books = [
-//   {
-//     title: "The Last Kingdom",
-//     author: "Bernard Cornwell",
-//     rating: 8.38,
-//   },
-//   {
-//     title: "Beside Still Waters",
-//     author: "Robert Sheckley",
-//     rating: 8.51,
-//   },
-//   {
-//     title: "The Dream of a Ridiculous Man",
-//     author: "Fyodor Dostoevsky",
-//     rating: 7.75,
-//   },
-//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-// ];
-
-// const MIN_RATING = 8;
-// const AUTHOR = "Bernard Cornwell";
-
-// const topRatedBooks = books.filter((book) => book.rating >= MIN_RATING);
-// const booksByAuthor = books.filter((book) => book.author === AUTHOR);
-// console.log(topRatedBooks);
-// console.log(topRatedBooks);
-
-const users = [
+[
   {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
     gender: "male"
   },
   {
     name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
     friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
     gender: "female"
   },
   {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    gender: "male"
-  },
-  {
     name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
     friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
     gender: "female"
   },
   {
     name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
     friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
     gender: "male"
   },
   {
     name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
     friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
     gender: "male"
   },
   {
     name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
     friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
     gender: "female"
+  },
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"],
+    gender: "male"
   }
 ]
-
-console.log(users);
-
-const getUsersWithEyeColor = (users, color) => {
-  return users.filter(user => user.eyeColor === color).map(user => user.name);
-};
